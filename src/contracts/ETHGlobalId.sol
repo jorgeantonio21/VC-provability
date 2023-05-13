bd58ce88559bdd1e5fec9938cbda1cc9d2183535
jorgeantonio21 <matroid@outlook.com>
2023-05-13 18:29:49 +0100

    finish template

--- src/contracts/ETHGlobalId.sol
+++ src/contracts/ETHGlobalId.sol
@@ -1,34 +1,85 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ZKPVerifier.sol";
+import "./Utils.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract ETHGlobalId {
-    uint public unlockTime;
-    address payable public owner;
+contract ETHGlobalPOAPContract is ERC20, ZKPVerifier {
+    enum ETHGlobalAttendeeResponse {
+        BAD,
+        AVERAGE,
+        GOOD,
+        GREAT,
+        AWESOME
+    }
 
-    event Withdrawal(uint amount, uint when);
+    uint64 public TRANSFER_REQUEST_ID = 1;
 
-    constructor(uint _unlockTime) payable {
+    mapping(address => uint256) public addressToId;
+    mapping(uint256 => address) public idToAddress;
+    mapping(uint256 => ETHGlobalAttendeeResponse)
+        public ETHGlobalAttendeeSurvey;
+
+    uint64[] public responseUserIds;
+
+    uint256 public TOKEN_AMOUNT_FOR_AIRDROP_PER_ID = 5 * 10 ** uint(decimals());
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {}
+
+    function _beforeProofSubmit(
+        uint64,
+        uint256[] memory inputs,
+        ICircuitValidator validator
+    ) internal view override {
+        // check that challenge input of the proof is equal to the msg.sender
+        address addr = Utils.int256ToAddress(
+            inputs[validator.getChallengeInputIndex()]
+        );
         require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
+            _msgSender() == addr,
+            "address in proof is not a sender address"
+        );
+    }
+
+    function _afterProofSubmit(
+        uint64 requestId,
+        uint256[] memory inputs,
+        ICircuitValidator validator
+    ) internal override {
+        require(
+            requestId == TRANSFER_REQUEST_ID && addressToId[_msgSender()] == 0,
+            "proof can not be submitted more than once"
         );
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        uint256 id = inputs[validator.getChallengeInputIndex()];
+        // execute the airdrop
+        if (idToAddress[id] == address(0)) {
+            super._mint(_msgSender(), TOKEN_AMOUNT_FOR_AIRDROP_PER_ID);
+            addressToId[_msgSender()] = id;
+            idToAddress[id] = _msgSender();
+        }
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function _beforeTokenTransfer(
+        address,
+        address to,
+        uint256
+    ) internal view override {
+        require(
+            proofs[to][TRANSFER_REQUEST_ID] == true,
+            "only identities who provided proof are allowed to receive tokens"
+        );
     }
+
+    function submitSurveyAttendee(
+        address attendeeAddress,
+        ETHGlobalAttendeeResponse response
+    ) internal override {}
 }
